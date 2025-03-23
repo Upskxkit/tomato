27bcb76a8a4d852154f463509a1b2ecb0d6e87ef
MS <upskxkit3@gmail.com>
2025-03-23 12:23:41 -0300

    add times store

---
+++ src/main/store.ts
@@ -0,0 +1,33 @@
+import { app } from 'electron'
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { Timer } from '../preload/types'
+
+const USER_DATA_PATH = path.join(app.getPath('userData'), 'user_data.json')
+const TIMER_DATA_PATH = path.join(app.getPath('userData'), 'timer_data.json')
+
+export async function readUserData() {
+  try {
+    const data = await fs.readFile(USER_DATA_PATH, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    console.log('Error retrieving user data', error)
+    // you may want to propagate the error, up to you
+    return null
+  }
+}
+
+export async function writeUserData(data) {
+  fs.writeFile(USER_DATA_PATH, JSON.stringify(data))
+}
+
+export async function writeTimerData(data: Timer[]) {
+  const jsonData = JSON.stringify(data)
+  fs.writeFile(TIMER_DATA_PATH, jsonData)
+}
+
+export async function readTimerData(): Promise<Timer[]> {
+  const data = await fs.readFile(TIMER_DATA_PATH, 'utf-8')
+
+  return JSON.parse(data)
+}
